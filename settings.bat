7e3a89cf7e41cc4cafe9a2e4de80eca32c752b03
Jon McManus <due@wxwhatever.com>
2019-06-29 21:42:21 +1000

    Stuff.

---
+++ settings.bat
@@ -0,0 +1,10 @@
+rem Do not touch
+set MCVER=1.12.2
+set JARFILE=minecraft_server.%MCVER%.jar
+set LAUNCHWRAPPERVERSION=1.12
+set LAUNCHWRAPPER=net\minecraft\launchwrapper\%LAUNCHWRAPPERVERSION%\launchwrapper-%LAUNCHWRAPPERVERSION%.jar
+set FORGEJAR=FTBserver-1.12.2-14.23.4.2738-universal.jar
+
+rem can be changed by user
+set MAX_RAM=4g
+set JAVA_PARAMETERS=-XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=5 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10
