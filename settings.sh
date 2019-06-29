e32f5de30c957bf1f9534de7c21bfb8703a536b8
Jon McManus <due@wxwhatever.com>
2019-06-29 21:56:16 +1000

    Oops.

---
+++ settings.sh
@@ -0,0 +1,26 @@
+###################################################
+# Configuration for ServerStart.sh and FTBInstall.sh
+#
+# TODO:
+# * updates for new mojang repositories
+# * create settings-local.sh automatically if missing
+# * document settings-local.sh creation and usage
+
+###################################################
+# Pack specific settings
+# Only edit this section if you know what you are doing
+
+export MCVER="1.12.2"
+export JARFILE="minecraft_server.${MCVER}.jar"
+export LAUNCHWRAPPERVERSION="1.12"
+export LAUNCHWRAPPER="net/minecraft/launchwrapper/${LAUNCHWRAPPERVERSION}/launchwrapper-${LAUNCHWRAPPERVERSION}.jar"
+export FORGEJAR="FTBserver-1.12.2-14.23.4.2738-universal.jar"
+
+###################################################
+# Default arguments for JVM
+
+## Copy following lines into settings-local.sh to create local config file which overrides default settings given here
+export JAVACMD="java"
+export MAX_RAM="3g"       # -Xmx
+export JAVA_PARAMETERS="-XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=5 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10"
+
